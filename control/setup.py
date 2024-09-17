dfb58e8544077f25636eb9d7616bf98137d4a74f
Ashley Neaves <ashley.neaves@stfc.ac.uk>
2024-09-17 14:08:59 +0100

    removing more files that were copied to the adxdma specific repo. Modified setup.py to remove references to cffi

--- control/setup.py
+++ control/setup.py
@@ -10,16 +10,14 @@ with open('requirements.txt') as f:
 
 required.append("cffi>=1.0.0")
 setup(name='babyd',
-    #   version=versioneer.get_versions(),
-    #   cmdclass=versioneer.get_cmdclass(),
+      version=versioneer.get_versions(),
+      cmdclass=versioneer.get_cmdclass(),
       description='ODIN Baby D',
       url='https://github.com/stfc-aeg',
       author='Ashley Neaves',
       author_email='ashley.neaves@stfc.ac.uk',
       packages=find_packages('src'),
       package_dir={'': 'src'},
-      setup_requires=["cffi>=1.0.0"],
-      cffi_modules=["../lib/pyxdma_builder.py:ffibuilder"],
       install_requires=required,
       zip_safe=False
 )
