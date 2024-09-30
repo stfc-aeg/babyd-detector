9abc2562342672feae0f8a6f402983cf8a765c8e
Ashley Neaves <ashley.neaves@stfc.ac.uk>
2024-09-30 09:16:43 +0100

    Modified Adxdma Controller adapter to include trigger and modify exisitng register maps for new firmware#

--- control/src/babyd/adxdma/controller.py
+++ control/src/babyd/adxdma/controller.py
@@ -6,6 +6,7 @@ import struct
 
 import logging
 
+
 class BabyDController(AlphaDataController):
 
     def __init__(self, reg_file):
@@ -14,31 +15,59 @@ class BabyDController(AlphaDataController):
         # add references to required registers for Acquisition Control
         self.ip_local = self.registers['UDP']['IP_LOCAL']
         self.ip_remote = self.registers['UDP']['IP_REMOTE']
-        self.data_rate = self.registers['AURORA']['CTRL']
-        self.link0_status = self.registers['AURORA']['CORE0_STATUS']
-        self.link1_status = self.registers['AURORA']['CORE1_STATUS']
+        # self.data_rate = self.registers['AURORA']['CTRL']
+        # self.link0_status = self.registers['AURORA']['CORE0_STATUS']
+        # self.link1_status = self.registers['AURORA']['CORE1_STATUS']
         self.clockA_period = self.registers["IIC"]["CLOCKA_PERIOD"]
         self.clockB_period = self.registers["IIC"]["CLOCKB_PERIOD"]
 
         self.digest = self.registers["AUTH"]["DIGEST"]
         self.dna = self.registers["AUTH"]["PUF_ID"]
 
-        self.clock_ref = self.registers["AURORA"]["REFCLK_FREQ"]
+        # self.clock_ref = self.registers["AURORA"]["REFCLK_FREQ"]
 
-        self.frame_count_ch0 = self.registers['FRAMER']['STATS_CH0_NFRAMES']
-        self.frame_count_ch1 = self.registers['FRAMER']['STATS_CH1_NFRAMES']
+        # self.frame_count_ch0 = self.registers['FRAMER']['STATS_CH0_NFRAMES']
+        # self.frame_count_ch1 = self.registers['FRAMER']['STATS_CH1_NFRAMES']
 
         self.mac_sent = self.registers["UDP"]["MAC_NFRAMES_SENT"]
+
+        self.frame_counts = {
+            map_num: {"ch0": map_ref['STATS_CH0_NFRAMES'],
+                      "ch1": map_ref["STATS_CH1_NFRAMES"]}
+            for (map_num, map_ref) in self.registers['FRAMER'].items()
+        }
+
+        self.data_rates = [
+            map_ref["CTRL"] for (map_num, map_ref) in self.registers['AURORA'].items()
+        ]
+
+        self.link_statuses = [
+            map_ref['CORE_STATUS'] for (map_num, map_ref) in self.registers['AURORA'].items()
+        ]
+
+        self.clock_refs = [
+            map_ref['REFCLK_FREQ'] for (map_num, map_ref) in self.registers['AURORA'].items()
+        ]
+
+        self.trigger_mode = self.registers['TRIGGER']['MODE']
+        self.trigger_frame_count = self.registers['TRIGGER']['PROG_N']
+        self.trigger_complex_val = self.registers['TRIGGER']['PROG_M']
 
         self.add_param("ip_local", (partial(self.get_ip_addr, register=self.ip_local),
                                     partial(self.set_ip_addr, register=self.ip_local)))
         self.add_param("ip_remote", (partial(self.get_ip_addr, register=self.ip_remote),
                                      partial(self.set_ip_addr, register=self.ip_remote)))
+        # self.add_param('link_status', {
+        #     "link0": {field_name: (partial(self.read_field, register=self.link0_status, field_addr=field_addr), None)
+        #               for field_name, field_addr in self.link0_status.fields.items()},
+        #     "link1": {field_name: (partial(self.read_field, register=self.link1_status, field_addr=field_addr), None)
+        #               for field_name, field_addr in self.link1_status.fields.items()}
+        # })
+
         self.add_param('link_status', {
-            "link0": {field_name: (partial(self.read_field, register=self.link0_status, field_addr=field_addr), None)
-                      for field_name, field_addr in self.link0_status.fields.items()},
-            "link1": {field_name: (partial(self.read_field, register=self.link1_status, field_addr=field_addr), None)
-                      for field_name, field_addr in self.link1_status.fields.items()}
+            reg_num: {field_name: (partial(self.read_field, register=reg_ref, field_addr=field_addr), None)
+                      for field_name, field_addr in reg_ref.fields.items()}
+            for (reg_num, reg_ref) in enumerate(self.link_statuses)
         })
 
         self.add_param("digest", (partial(self.read_reg, register=self.digest), None))
@@ -55,15 +84,31 @@ class BabyDController(AlphaDataController):
              14: -1
         }
 
+        self.trigger_options = {
+            "default": 0,
+            "high": 1,
+            "low": 2,
+            "frames": 3,
+            "complex": 4,
+            "off": 7
+        }
+
         self.add_param("clock_speed", {
             "options": (list(self.clock_speeds.keys()), None),
             "speed": (self.get_clock_speed, self.set_clock_speed)
         })
 
-        self.add_param("frame_count", {
-            "ch0": (partial(self.read_reg_value, register=self.frame_count_ch0), None),
-            "ch1": (partial(self.read_reg_value, register=self.frame_count_ch1), None)
+        self.add_param("trigger", {
+            "options": (list(self.trigger_options.keys()), None),
+            "mode": (self.get_trigger_mode, self.set_trigger_mode),
+            "frame_per_event": (partial(self.read_reg_value, register=self.trigger_frame_count),
+                                self.write_trigger_per_event)
         })
+
+        self.add_param("frame_count", {
+            map_num: {chan_name: (partial(self.read_reg_value, register=chan_ref), None)
+                      for (chan_name, chan_ref) in map_ref.items()}
+            for (map_num, map_ref) in self.frame_counts.items()})
 
         # self.add_param("mac_sent", (partial(self.read_reg_value, register=self.mac_sent), None))
 
@@ -83,8 +128,8 @@ class BabyDController(AlphaDataController):
             raise ParameterTreeError(e)
 
     def get_clock_speed(self):
-        rate_val = self.read_field(self.data_rate, (16, 17))
-        clock_ref = self.read_reg(self.clock_ref)[0]
+        rate_val = self.read_field(self.data_rates[0], (16, 17))
+        clock_ref = self.read_reg(self.clock_refs[0])[0]
         if rate_val == 0:
             return 14.0
         elif rate_val == 1:
@@ -101,17 +146,32 @@ class BabyDController(AlphaDataController):
         logging.debug("Setting Clock Speed to %d", speed)
         if speed == 14:
             logging.debug("clock speed 14")
-            self.write_field(0, self.data_rate, (16, 17))
+            for data_rate in self.data_rates:
+                self.write_field(0, data_rate, (16, 17))
         elif speed == 7:
             logging.debug("clock speed 7")
-            self.write_field(1, self.data_rate, (16, 17))
+            for data_rate in self.data_rates:
+                self.write_field(1, data_rate, (16, 17))
         else:
             try:
                 logging.debug("clock speed special")
-                self.write_field(2, self.data_rate, (16, 17))
-                self.write_reg([self.clock_speeds[speed]], self.clock_ref)
+                for data_rate in self.data_rates:
+                    self.write_field(2, data_rate, (16, 17))
+                for clock_ref in self.clock_refs:
+                    self.write_reg([self.clock_speeds[speed]], clock_ref)
             except ValueError as e:
                 raise ParameterTreeError(e)
+
+    def get_trigger_mode(self):
+        trigger_val = self.read_field(self.trigger_mode, (0, 3))
+        return {v: k for k, v in self.trigger_options.items()}.get(trigger_val, "unknown")
+
+    def set_trigger_mode(self, value):
+        trigger_val = self.trigger_options.get(value)
+        self.write_field(trigger_val, self.trigger_mode, (0, 3))
+
+    def write_trigger_per_event(self, value):
+        self.write_reg([value], self.trigger_per_event)
 
     def read_reg_value(self, register):
         val = self.read_reg(register)
